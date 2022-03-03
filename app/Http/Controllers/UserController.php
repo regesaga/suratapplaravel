6c826c603648fc420910e4dc14e29dd0714a40da
regesaga <regesaga@hotmail.com>
2022-03-03 19:41:14 +0700

    menambahkan route logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/user/logout', [UserController::Class, 'logout'])->name('logout.user');
 Route::get('/', function () {
     return view('welcome');
 });
